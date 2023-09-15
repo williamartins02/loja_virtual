24ab4ad4fa26db90f354a80c59cfd6f99d677fee
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-15 14:57:19 -0300

    Classe Acesso

---
+++ src/main/java/com/lojavirtual/model/Acesso.java
@@ -0,0 +1,35 @@
+package com.lojavirtual.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter @EqualsAndHashCode
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+//Class de autorização do spring security
+public class Acesso implements GrantedAuthority {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+	@Column(nullable = false)//Anotacao para dizer que descricao precisa ser obrigatorio
+	private String descricao; // Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	
+}
