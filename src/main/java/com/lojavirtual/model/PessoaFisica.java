2d1706eeeb3773b73cfbaa6e4995eca0b8d39ea0
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-15 16:49:56 -0300

    Class PessoaFisica para extend Class Pessoa.

---
+++ src/main/java/com/lojavirtual/model/PessoaFisica.java
@@ -0,0 +1,30 @@
+package com.lojavirtual.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+//Class Extend class Pessoa
+@Getter @Setter @ToString 
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(unique = true)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+}
