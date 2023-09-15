9a6bcc59b5c7bbe7ec98ac04749cf9c696d61e5f
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-15 16:48:55 -0300

    Criando class PessoaJuridcia Extend Class Pessoa

---
+++ src/main/java/com/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,29 @@
+package com.lojavirtual.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+//Class Extend class Pessoa
+
+@Getter @Setter @ToString 
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(unique = true)
+	private String cnpj;
+	private String inscEstadual;
+	private String inscMunicipal;
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String categoria;
+}
