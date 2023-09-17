971386c90ab82e5aa71b51a22b5f017c02551c2d
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-16 22:33:39 -0300

    criando class intem vendas, criando associação ManyToOne PRoduto e venda loja virtual

---
+++ src/main/java/com/lojavirtual/model/ItemVendaLoja.java
@@ -0,0 +1,52 @@
+package com.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @EqualsAndHashCode
+@Entity
+@Table(name = "item_venda_loja")
+@SequenceGenerator(name = "seq_item_venda_loja", sequenceName = "seq_item_venda_loja", allocationSize = 1, initialValue = 1)
+public class ItemVendaLoja implements Serializable {
+
+private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_item_venda_loja")
+	private Long id;
+	
+	private Double quantidade;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(
+			    value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+	private Produto produto;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = @ForeignKey(
+			    value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk"))
+	private VendaCompraLojaVirt vendaCompraLojaVirt;
+	
+	
+	
+	
+}
