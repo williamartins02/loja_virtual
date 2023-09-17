c0e22d5a7446478a434e63555913b615c2b20182
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-16 21:22:48 -0300

    criando class de nota fiscal venda.

---
+++ src/main/java/com/lojavirtual/model/NotaFiscalVenda.java
@@ -0,0 +1,40 @@
+package com.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @EqualsAndHashCode
+@Entity
+@Table(name = "nota_fiscal_venda")
+@SequenceGenerator(name = "seq_nota_fiscal_venda", sequenceName = "seq_nota_fiscal_venda", allocationSize = 1, initialValue = 1)
+public class NotaFiscalVenda implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_venda")
+	private Long id;
+	private String numero;
+	private String serie;
+	private String tipo;
+	
+	@Column(columnDefinition = "text")
+	private String xml;
+	
+	@Column(columnDefinition = "text")
+	private String pdf;
+
+}
