c5d9e37a74e1b57e6f19db6719aa01591eae19d9
williamartins02 <willi@DESKTOP-GCTU1FK.intelbras.local>
2023-09-19 18:48:14 -0300

    ajuste aplication properties.

--- src/test/java/com/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/com/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,13 +1,33 @@
 package com.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualApplicationTests {
+import com.lojavirtual.model.Acesso;
+import com.lojavirtual.repository.AcessoRepository;
+import com.lojavirtual.service.AcessoService;
+
+@SpringBootTest(classes = LojaVirtualApplication.class)
+public class LojaVirtualApplicationTests {
+	
+	@Autowired
+	private AcessoService acessoService;
+	@Autowired
+	private AcessoRepository acessoRepository;
 
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		acesso.setDescricao("ROLE_SECRETARIA");
+		
+		
+		acessoRepository.save(acesso);
 	}
 
+	
+	
 }
